ce789b3fa261acc16c55c051c20a50a4f0775916
FedorowPavel <pavel.fedorov@exposit.com>
2022-03-18 16:42:14 +0300

    redux with typescript boilerplate

---
+++ redux-practice-typescript/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import UserList from "./components/UserList";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <UserList/>
+      <hr/>
+      <TodoList/>
+
+    </div>
+  );
+}
+
+export default App;
